6c543848b32ee0a3ebeb1c4d88ad1aeb300fcd68
Nithin Raj V <146872871+nithinraj1999@users.noreply.github.com>
2025-04-12 14:03:30 +0530

    Delete dist directory

--- dist/model/categoryModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categoryModel = void 0;
-const mongoose_1 = require("mongoose");
-const CategorySchema = new mongoose_1.Schema({
-    categoryName: { type: String, required: true, unique: true },
-    description: { type: String, required: true },
-}, { timestamps: true });
-exports.categoryModel = (0, mongoose_1.model)("Category", CategorySchema);
